957bc2c78d40606b820df14527a3981107f5deb2
zivoradserafimoskitech <zhivorad@serafimoski.tech>
2026-09-01 15:35:01 +0200

    Delete repo-overlay directory

--- repo-overlay/python-service/retrain/__init__.py
+++
@@ -1,5 +0,0 @@
-"""VoltTrade nightly retrain pipeline (Phase 2)."""
-
-from retrain.pipeline import run_retrain, DRIFT_THRESHOLD, PROMOTION_MIN_IMPROVEMENT
-
-__all__ = ["run_retrain", "DRIFT_THRESHOLD", "PROMOTION_MIN_IMPROVEMENT"]
